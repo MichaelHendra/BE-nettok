e2daaac8694f103b680a289323d4a0ce7baeba8c
MichaelHendra <michaelhendranata01@gmail.com>
2024-07-13 11:08:21 +0700

    Fixing Logout Stuck After Not Using For a While

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -3,19 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class LogoutController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $removeToken = JWTAuth::invalidate(JWTAuth::getToken());
+        try{
+            if(! $token = JWTAuth::getToken()){
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Token not Provided'
+                ], 400);            
+            }
+            $removeToken = JWTAuth::invalidate($token);
 
-        if($removeToken){
-            return response() -> json ([
-                'successs' => true,
+            return response()->json([
+                'success' => true,
                 'message' => 'Logout Berhasil'
             ]);
+        }catch(JWTException $e){
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed to logout, please try again.',
+                'error' => $e->getMessage()
+            ], 500);
         }
     }
 }
